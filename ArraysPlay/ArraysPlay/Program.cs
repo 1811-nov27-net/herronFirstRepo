67a53cf466b6f78d423deb926dc301d236597234
Daniel Herron <heatwaveqc@yahoo.com>
2018-11-29 11:59:37 -0600

    Collections stuff

---
+++ ArraysPlay/ArraysPlay/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ArraysPlay
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Arrays();
+        }
+
+        static void Arrays()
+        {
+            int[] intArray = new int[10];
+            /*
+             * default null for objects, 0 for ints, false for bool
+            */
+            Console.WriteLine(intArray[5]);
+
+            for (int i = 0; i < intArray.Length; i++)
+            {
+                Console.Write(intArray[i]);
+
+            }
+
+            foreach (int item in intArray)
+            {
+                Console.Write(item);
+            }
+            Console.WriteLine();
+
+            /*
+             * can use foreach with anything that implements IEnumerable interface. (Also used with LINQ)
+             * 
+             */
+
+            int[,] trueMultiDArray = new int[5, 5];
+
+            trueMultiDArray[3, 4] = 5;
+
+            int[] array = new int[]
+            {
+                4, 8, 3, 2, 1, 2
+            };
+        }
+
+        static void Lists()
+        {
+            var list = new List<bool>
+            {
+                true,
+                true,
+                false
+            };
+
+            var list2 = new List<bool>() { false, false, false };
+
+            list2.AddRange(list);
+
+
+        }
+
+        static void sets()
+        {
+            var set = new HashSet<string>
+            {
+
+                // sets have no particular order to them.
+                // do not allow duplicates
+
+                "abc",
+                "abc", // does nothing
+                "def"
+            };
+            string conCats = "abc" + "def";
+        }
+
+        static void Dictionaries()
+        {
+            var dict = new Dictionary<string, string>
+            {
+                { "Germany", "Berlin" },
+                { "USA", "Washington, DC" }
+            };
+
+            Console.WriteLine(dict["USA"]);
+            dict["Mexico"] = "Mexico City";
+        }
+    }
+}
--- ExceptionHandling/ExceptionHandling/Program.cs
+++ ExceptionHandling/ExceptionHandling/Program.cs
@@ -6,7 +6,44 @@ namespace ExceptionHandling
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Exceptions are runtime errors that might be handled
+            // they are objects, and defined by objects... but they are "special"
+
+            try
+            {
+                BadCode();
+
+                // now never runs; BadCode
+                var x = 4;
+                var y = x / 0;
+                Console.WriteLine("Can't see me, I'm the gingerbread man");
+            }
+            catch (DivideByZeroException e)
+            {
+                Console.WriteLine("divided by zero, moving on");
+            }
+            catch(InvalidCastException e)
+            {
+                Console.WriteLine("Handled Bad Cast");
+            }
+
+            
+
+            Console.WriteLine("the saga continues");
+        }
+
+        static void BadCode()
+        {
+            try
+            {
+                object o = true;
+                string s = (string)o;
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Don't divide by zero");
+
+            }
         }
     }
 }

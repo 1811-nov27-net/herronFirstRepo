cc77cfff4e99520aff5c705a3309e5a78564431f
Daniel Herron <heatwaveqc@yahoo.com>
2018-12-06 11:48:00 -0600

    Made all the things.

--- ADLNETDisconected/ADLNETDisconected/Program.cs
+++ ADLNETDisconected/ADLNETDisconected/Program.cs
@@ -1,20 +1,14 @@
 ﻿using System;
+using System.Data;
 using System.Data.SqlClient;
 
-namespace ADONetConnecting
+namespace ADLNETDisconected
 {
     class Program
     {
         static void Main(string[] args)
         {
-            // ADO.NET is technically MS's umbrella name for all data-related libraries
-            // incl Entity Framework
-            // but when we say ADO.NET we typically are talking about older stuff.
-            // DataReader, DataAdapter objects
-
-            // in various GUIs, you need server URL, login, password
-            // in code, we have developed a convention to use what we call a connection string
-            // which will jam all that data into one string
+            
 
             // never commit your connection strings to source control like git.
             var ConnectionString = SecretConfiguration.ConnectionString;
@@ -29,34 +23,47 @@ namespace ADONetConnecting
             // or you'll get haxxored
 
 
-            // connected architecture - recieve the whole result
-            // have it waiting in the network buffer
-            // and use a "cursor"/iterator to read it in row by row
+            // disconnected architecture - get it all
+            // load into dataset
+            // process
 
-            using (var connection = new SqlConnection(ConnectionString)) 
+            // more overhead on the C# side but keeps connection open for less time
+            // which is good because the DB is usually the bottleneck
+
+            var dataSet = new DataSet(); // will hold our results.
+
+            using (var connection = new SqlConnection(ConnectionString))
             {
-                // connected architecture 
+                // disconnected architecture 
                 // step 1: open the connection
                 connection.Open();
                 // step 2: execute query
                 using (var command = new SqlCommand(commandString, connection))
                 // command.ExecuteReader for SELECT queries that return things
                 // command.ExecuteNonQuery for all other commands
-                using (SqlDataReader reader = command.ExecuteReader())
+                using (var adaptor = new SqlDataAdapter(command))
                 {
-                    // step 3 process results
-                    while (reader.Read())
-                    {
-                        var id = reader["ID"]; // access values by column name
-                        var name = reader["Name"];
-                        Console.WriteLine($"ID: {id}, Name: {name}");
-                    }
+                    // step 3: fill dataset
+                    adaptor.Fill(dataSet);
+                    // still uses DataReader object internally
                 }
                 // step 4: close the connection
                 connection.Close();
 
             }
 
+            // step 5 process results
+            var firstTable = dataSet.Tables[0];
+            // foreach without generics does a cast when you assign a type here
+            
+            foreach (DataRow row in firstTable.Rows)
+            {
+                // DataSet contains DataTable, DataColumn, DataRow, etc.
+
+                object id = row["id"];
+                object name = row["Name"];
+                Console.WriteLine($"ID: {id}, Name: {name}");
+            }
 
         }
     }

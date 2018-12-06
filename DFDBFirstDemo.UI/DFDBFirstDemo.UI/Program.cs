2dc861a9742327f7158cf3aee0b005407d29742b
Daniel Herron <heatwaveqc@yahoo.com>
2018-12-06 14:16:26 -0600

    Made the stuff.

---
+++ DFDBFirstDemo.UI/DFDBFirstDemo.UI/Program.cs
@@ -0,0 +1,34 @@
+﻿using EFDBFirstDemo.DataAccess;
+using EFDBFirstDemo.UI;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DFDBFirstDemo.UI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var connectionString = SecretConfiguration.ConnectionString;
+
+            var optionsBuilder = new DbContextOptionsBuilder<MoviesDBContext>();
+
+            optionsBuilder.UseSqlServer(connectionString);
+            var options = optionsBuilder.Options;
+
+            var movies = new List<Movie>();
+            using (var db = new MoviesDBContext(options))
+            {
+                movies = db.Movie.ToList();
+            }
+
+            foreach (var item in movies)
+            {
+                Console.WriteLine($"movie ID{item.Id}, name {item.Name}");
+            }
+            
+        }
+    }
+}

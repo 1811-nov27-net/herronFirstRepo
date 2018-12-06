cdbb2ebedfdf093d6de828dfd113db502baf14c7
Daniel Herron <heatwaveqc@yahoo.com>
2018-12-06 11:17:39 -0600

    SQL links established

--- ADONetConnecting/ADONetConnecting/Program.cs
+++ ADONetConnecting/ADONetConnecting/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Data.SqlClient;
 
 namespace ADONetConnecting
 {
@@ -19,6 +20,29 @@ namespace ADONetConnecting
             var ConnectionString = SecretConfiguration.ConnectionString;
 
             var commandString = "SELECT * FROM Movies.Movie";
+
+            using (var connection = new SqlConnection(ConnectionString)) 
+            {
+                // connected architecture 
+                // step 1: open the connection
+                connection.Open();
+                // step 2: execute query
+                using (var command = new SqlCommand(commandString, connection))
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    // step 3 process results
+                    while (reader.Read())
+                    {
+                        var id = reader["ID"]; // access values by column name
+                        var name = reader["Name"];
+                        Console.WriteLine($"ID: {id}, Name: {name}");
+                    }
+                }
+                // step 4: close the connection
+                connection.Close();
+
+            }
+
 
         }
     }

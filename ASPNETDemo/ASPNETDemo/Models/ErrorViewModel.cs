76b59f2f5718e4b0b3346b483e5ffc4cad56e0ef
Daniel Herron <heatwaveqc@yahoo.com>
2018-12-12 10:05:21 -0600

    ASPNET demo created.

---
+++ ASPNETDemo/ASPNETDemo/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ASPNETDemo.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

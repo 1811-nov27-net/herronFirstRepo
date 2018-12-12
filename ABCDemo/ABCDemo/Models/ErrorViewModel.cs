ced8b0c5d02b463b219637d85b8651cdcccee0f8
Daniel Herron <heatwaveqc@yahoo.com>
2018-12-12 10:16:16 -0600

    Add generated code.

---
+++ ABCDemo/ABCDemo/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ABCDemo.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

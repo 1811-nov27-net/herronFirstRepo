c61352aca1547f0bee4fd5997a3b81c52f9f51b7
Daniel Herron <heatwaveqc@yahoo.com>
2018-12-19 11:54:41 -0600

    "notes" from class.

---
+++ ApiThing/ApiThing/Controllers/ValuesController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ApiThing.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiThing.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        // really, we'd use a database
+        public static List<Temperature> Data = new List<Temperature>();
+        
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<Temperature>> Get()
+        {
+            return Data;
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<Temperature> Get(int id)
+        {
+            var ret = Data.FirstOrDefault(x => x.TempId == id);
+            if (ret == null)
+            {
+                NotFound();
+            }
+            return ret;
+        }
+
+        // POST api/values
+        // inserting a new resource
+        [HttpPost]
+        public void Post([FromBody] Temperature value)
+        {
+            Data.Add(value);
+        }
+
+        // PUT api/values/5
+        // replace existing resource
+        [HttpPut("{id}")]
+        public ActionResult Put(int id, [FromBody] Temperature value)
+        {
+            var existing = Data.FirstOrDefault(x => x.TempId == id);
+            if (existing != null)
+            {
+                Data.Remove(existing);
+
+            }
+            Data.Add(value);
+            return Ok();
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public ActionResult Delete(int id)
+        {
+            var existing = Data.FirstOrDefault(x => x.TempId == id);
+            if (existing != null)
+            {
+                Data.Remove(existing);
+
+            }
+            return Ok();
+        }
+    }
+}
---
+++ ApiThing/ApiThing/Models/Temperature.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiThing.Models
+{
+    public class Temperature
+    {
+        [Required]
+        public int TempId { get; set; }
+        [Required]
+        public DateTime Time { get; set; }
+        [Required]
+        [Range(-200,1000)]
+        public double CelsiusValue { get; set; }
+    }
+
+    public enum TempType
+    {
+        Celsius,
+        Ferenheight,
+        Kelvin
+    }
+}
